d527bc23174a74ac969270354ef8c4d42a421433
Danila Kalay <ChechikLud@gmail.com>
2022-06-15 14:47:12 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        int balance = 200;
+        int addBalance = 1020;
+        int bonus;
+
+        if (addBalance < 1000) {
+            bonus = 0;
+        } else { bonus = addBalance / 100;
+        }
+        int newBalance = balance + addBalance + bonus;
+
+
+        System.out.println("Наш баланс = " + newBalance);
+    }
+}
